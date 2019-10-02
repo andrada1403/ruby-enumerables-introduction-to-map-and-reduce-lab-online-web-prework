5889882d06762eccb65b575e73f223b2d478aea7
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-10-02 15:59:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -37,12 +37,12 @@ def map_to_square(array)
 final_array
 end
 
-def reduce_to_total(array)
-  final_array=[]
+def reduce_to_total(array, starting_point=0)
+  total=starting_point
   counter=0
   while counter<array.size do
-    final_array.push(array[counter])
+    
     counter+=1
   end
-final_array
+total
 end
