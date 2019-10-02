f178326fad2c119b939c66a5cb58866a763a467d
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-10-02 15:32:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,48 @@
-# My Code here....
+def map_to_negativize(array)
+  final_array=[]
+  counter=0
+  while counter<array.size do
+    final_array.push(array[counter]*-1)
+    counter+=1
+  end
+final_array
+end
+
+def map_to_no_change(array)
+  final_array=[]
+  counter=0
+  while counter<array.size do
+    final_array.push(array[counter])
+    counter+=1
+  end
+final_array
+end
+
+def map_to_double(array)
+  final_array=[]
+  counter=0
+  while counter<array.size do
+    final_array.push(array[counter]*2)
+    counter+=1
+  end
+final_array
+end
+def map_to_square(array)
+  final_array=[]
+  counter=0
+  while counter<array.size do
+    final_array.push(array[counter]**2)
+    counter+=1
+  end
+final_array
+end
+
+def reduce_to_totals(array)
+  final_array=[]
+  counter=0
+  while counter<array.size do
+    final_array.push(array[counter])
+    counter+=1
+  end
+final_array
+end
