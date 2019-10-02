5329d89411e00ff03be8def99f68413bde20c8f0
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-10-02 16:15:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -55,3 +55,12 @@ def reduce_to_all_true(array)
   end
   return true
 end
+
+def reduce_to_any_true(array)
+  counter=0
+  while counter<array.size do
+    return false if array[counter]==false
+    counter+=1
+  end
+  return true
+end
